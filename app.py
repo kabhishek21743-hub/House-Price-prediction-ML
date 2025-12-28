c47d181c0cd141ef703dab5c1c685cd3749929e7
kabhishek21743-hub <kabhishek21743@gmail.com>
2025-12-28 16:03:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -30,6 +30,10 @@ model = RandomForestRegressor()
 model.fit(X,y)
 house_price = model.predict(final_value)
 with st.spinner('Predicting House price'):
-  time.sleep(3)
-st.write(house_price)
+  time.sleep(1)
+msg = f'''House price is: ${house_price*100000}'''
+st.success(msg)
+
+
+st.markdown('''**Design and Developed by: Abhishek kumar***''')
 
