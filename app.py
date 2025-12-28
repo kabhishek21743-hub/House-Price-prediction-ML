614d38f45706aa0e8bd105d117e97dc4b2ccdb4e
kabhishek21743-hub <kabhishek21743@gmail.com>
2025-12-28 14:59:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,4 +5,5 @@ import pandas as pd
 import time
 from sklearn.datasets import fetch_california_housing
 st.title('🏠 House Price prediction using ML')
+st.image('https://i.pinimg.com/originals/46/cb/a9/46cba9f93a3d437db6d42f4bcd1a5f5f.gif')
 
